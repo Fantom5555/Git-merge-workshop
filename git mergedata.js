dc3a272b80f46196e1368a810b29ec55bd911a7c
Sos-V <sosyan.vahe@gmail.com>
2023-02-28 11:57:31 +0100

    änderung 4

--- git mergedata.js
+++ git mergedata.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 Hallo Vahe bitte code auf Zeile 1 anschauen.
 console.log("Hallo Fati , okey") 
 let x= 5;
-=======
-Hey Vahe bitte code auf Zeile 1 anschauen.
->>>>>>> 9d8344f4d2fc2cafdc3fab98e1597de490271a68
-=======
-ich bins Fateh code auf Zeile 1 anschauen.
->>>>>>> 69abb9745658b1da16c611a1f657a02a5b2c27d9
+
+
+console.log("mache ich 3 Änderung");
+Hallo Fati jetz mache ich probleme
